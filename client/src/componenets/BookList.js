4727e546c14015ea715a1fe6d4d1d8828a361789
Soulyana <soulyana.lakew@accenturefederal.com>
2019-07-17 18:42:26 -0400

    rendering data in a component

--- client/src/componenets/BookList.js
+++ client/src/componenets/BookList.js
@@ -1,29 +1,34 @@
-import React, {Component} from 'react';
-import { gql } from 'apollo-boost';
-import {graphql} from 'react-apollo';
+import React, { Component } from "react";
+import { gql } from "apollo-boost";
+import { graphql } from "react-apollo";
 
 const getBooksQuery = gql`
-    {
-        books{
-            name
-            id
-
-        }
+  {
+    books {
+      name
+      id
     }
-`
+  }
+`;
 class BookList extends Component {
-    render() {
-        console.log(this.props);
-        return (
-            <div>
-                <ul id="book-list">
-                    <li>
-                        Book Name
-                    </li>
-                </ul>
-            </div>
-        )
-    };
+  displayBooks() {
+    var data = this.props.data;
+    if (data.loading) {
+      return <div>Loading books...</div>;
+    } else {
+      return data.books.map(book => {
+        return <li key={book.id}>{book.name}</li>;
+      });
+    }
+  }
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <ul id="book-list">{this.displayBooks()}</ul>
+      </div>
+    );
+  }
 }
 
 export default graphql(getBooksQuery)(BookList);
