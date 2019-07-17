2d654a2f1f4ccc04a9ea56714a7eb7149d254bba
Soulyana <soulyana.lakew@accenturefederal.com>
2019-07-17 18:26:12 -0400

    Apollo setup in client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
 //components
 import BookList from "./componenets/BookList";
 
+// Apollo Client Setup
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
 function App() {
   return (
-    <div id="main">
-      <h1>Soul's Reading List</h1>
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>Soul's Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
