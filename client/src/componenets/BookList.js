4895934549f88e89d6350a3b5bbb7d6c14ea5db5
Soulyana <soulyana.lakew@accenturefederal.com>
2019-07-17 19:48:13 -0400

    re-fetching queries for fast-reload of updated data

--- client/src/componenets/BookList.js
+++ client/src/componenets/BookList.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
 import { getBooksQuery } from "../queries/queries";
+
 class BookList extends Component {
   displayBooks() {
     var data = this.props.data;
