6171ba66fbbc59f06828b4771e5dcad61b5b2e76
Soulyana <soulyana.lakew@accenturefederal.com>
2019-07-17 15:44:27 -0400

    added relationship between book and author

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -10,9 +10,9 @@ const { GraphQLObjectType,
 
 // dummy data
 let books = [
-  { name: "Name of the Wind", genre: "Fantasy", id: "1" },
-  { name: "The Final Empire", genre: "Fantasy", id: "2" },
-  { name: "The Long Earth", genre: "Sci-Fi", id: "3" }
+  { name: "Name of the Wind", genre: "Fantasy", id: "1" , authorId: '1'},
+  { name: "The Final Empire", genre: "Fantasy", id: "2" , authorId: '2'},
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: '3' }
 ];
 
 let authors = [
@@ -26,7 +26,14 @@ const BookType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString }
+    genre: { type: GraphQLString },
+    author: {
+        type: AuthorType,
+        resolve(parent, args){
+            console.log(parent);
+            return _.find(authors, {id:parent.authorId});
+        }
+    }
   })
 });
 
