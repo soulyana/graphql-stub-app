8cd68e20da892263a14cfb267ee4976a546dd125
Soulyana <soulyana.lakew@accenturefederal.com>
2019-07-17 15:26:46 -0400

    update id to GraphQLID

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,19 +1,23 @@
 const graphql = require("graphql");
-const _ = require('lodash');
+const _ = require("lodash");
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, 
+    GraphQLString, 
+    GraphQLSchema,
+    GraphQLID
+ } = graphql;
 
 // dummy data
 let books = [
   { name: "Name of the Wind", genre: "Fantasy", id: "1" },
   { name: "The Final Empire", genre: "Fantasy", id: "2" },
-  { name: 'The Long Earth', genre: 'Sci-Fi', id:'3'}
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3" }
 ];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
   })
@@ -24,10 +28,10 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // code to get data from db / other source
-        return _.find(books, {id:args.id});
+        return _.find(books, { id: args.id });
       }
     }
   }
